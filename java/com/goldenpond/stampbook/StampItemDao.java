84b0515c5abf3dcf02971c08370bea6baee036f7
matthew <matthew.green.lee@gmail.com>
2012-07-05 17:06:28 +0800

    create Dao class and pull up common variables and methods

--- java/com/goldenpond/stampbook/StampItemDao.java
+++ java/com/goldenpond/stampbook/StampItemDao.java
@@ -1,15 +1,9 @@
 package com.goldenpond.stampbook;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
-public class StampItemDao {
-
-	private Connection conn;
-	private Statement stmt;
+public class StampItemDao extends Dao{
 
 	public void create(StampItem item) {
 		String sql = "insert into STAMP_ITEM(ISSUE_NUMBER, NAME, ISSUE_DATE, DESIGNED_BY, PRINTED_BY) " +
@@ -48,43 +42,5 @@ public class StampItemDao {
 		String sql = "delete from STAMP_ITEM where ISSUE_NUMBER='" + item.getIssueNumber() + "'";
 		int rowCnt = executeUpdate(sql);
 		System.out.println(rowCnt + " row(s) were deleted");
-	}
-
-	private int executeUpdate(String sql) {
-		try {
-			return stmt.executeUpdate(sql);
-		} catch (SQLException e) {
-			System.out.println("failed to execute the given sql [" + sql + "]");
-			throw new RuntimeException(e);
-		}
-	}
-
-	private ResultSet executeQuery(String sql) {
-		try {
-			return stmt.executeQuery(sql);
-		} catch (SQLException e) {
-			System.out.println("failed to execute the given sql [" + sql + "]");
-			throw new RuntimeException(e);
-		}
-	}
-
-	public void connect() {
-		try {
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/stampbook", "root", "root");
-			stmt = conn.createStatement();
-		} catch (SQLException e) {
-			System.out.println("failed to connect to database");
-			throw new RuntimeException(e);
-		}
-	}
-
-	public void disconnect() {
-		try {
-			stmt.close();
-			conn.close();
-		} catch (SQLException e) {
-			System.out.println("failed to disconnect with database");
-			throw new RuntimeException(e);
-		}
 	}
 }
