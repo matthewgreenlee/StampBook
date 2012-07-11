a0bb8b298d3555956dca020b4d8872d5bbc649c7
matthew <matthew.green.lee@gmail.com>
2012-07-11 11:01:15 +0800

    change mapping table name to "STAMP"

--- java/com/goldenpond/stampbook/StampItem.java
+++ java/com/goldenpond/stampbook/StampItem.java
@@ -11,7 +11,7 @@ import javax.persistence.Table;
 import org.hibernate.annotations.GenericGenerator;
 
 @Entity
-@Table(name = "STAMP_ITEM")
+@Table(name = "STAMP")
 public class StampItem {
 
 	private long id;
