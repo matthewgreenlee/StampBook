d93f5b289b8757bbd09565a99bc4ba733a84b4ad
matthew <matthew.green.lee@gmail.com>
2012-10-26 16:43:35 +0800

    show ajax response for POST stamps request

--- WebContent/catalog.js
+++ WebContent/catalog.js
@@ -9,7 +9,7 @@ getStamps = function() {
         success: function(response) {
             var resp = response.stamp;
             jQuery.each(resp, function(i, data) {
-                $("#CatalogArea").append("<tr><td>" + data.issueNumber + "</td><td>" + data.name + "</td></tr>");
+                $("#CatalogArea").prepend("<tr><td>" + data.issueNumber + "</td><td>" + data.name + "</td><td><input type='button' value='Edit'></td></tr>");
             });
         }
     });
@@ -24,8 +24,13 @@ addStamp = function() {
     		name: $("#name").val(),
     		type: $("#type").val()
     	},
-    	success: function() {
-    	    alert("addition done");    		
+    	success: function(response) {
+    	    var type = $("#issueNumber").val().charAt(0);
+    	    var number = $("#issueNumber").val().substr(1);
+    	    $("#issueNumber").val(type + (++number));
+    	    $("#name").val("");
+    	    
+            $("#CatalogArea").prepend("<tr><td>" + response.issueNumber + "</td><td>" + response.name + "</td><td><input type='button' value='Edit'></td></tr>");
     	}
     });
 };
